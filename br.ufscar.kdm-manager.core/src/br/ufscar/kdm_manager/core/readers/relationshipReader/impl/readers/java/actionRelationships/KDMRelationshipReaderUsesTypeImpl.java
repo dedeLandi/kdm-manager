ce73238f23efc12ee640ac56c71514dcad98ea1c
André <a_souzalandy@yahoo.com.br>
2016-10-13 19:45:55 -0300

    Atividade 61 - Reader de Relações do tipo UsesType

---
+++ br.ufscar.kdm-manager.core/src/br/ufscar/kdm_manager/core/readers/relationshipReader/impl/readers/java/actionRelationships/KDMRelationshipReaderUsesTypeImpl.java
@@ -0,0 +1,238 @@
+package br.ufscar.kdm_manager.core.readers.relationshipReader.impl.readers.java.actionRelationships;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.gmt.modisco.omg.kdm.action.ActionElement;
+import org.eclipse.gmt.modisco.omg.kdm.action.BlockUnit;
+import org.eclipse.gmt.modisco.omg.kdm.action.UsesType;
+import org.eclipse.gmt.modisco.omg.kdm.code.AbstractCodeElement;
+import org.eclipse.gmt.modisco.omg.kdm.code.AbstractCodeRelationship;
+import org.eclipse.gmt.modisco.omg.kdm.code.ClassUnit;
+import org.eclipse.gmt.modisco.omg.kdm.code.CodeItem;
+import org.eclipse.gmt.modisco.omg.kdm.code.CodeModel;
+import org.eclipse.gmt.modisco.omg.kdm.code.EnumeratedType;
+import org.eclipse.gmt.modisco.omg.kdm.code.InterfaceUnit;
+import org.eclipse.gmt.modisco.omg.kdm.code.MethodUnit;
+import org.eclipse.gmt.modisco.omg.kdm.code.Package;
+import org.eclipse.gmt.modisco.omg.kdm.code.ParameterUnit;
+import org.eclipse.gmt.modisco.omg.kdm.code.Signature;
+import org.eclipse.gmt.modisco.omg.kdm.code.StorableUnit;
+import org.eclipse.gmt.modisco.omg.kdm.kdm.Segment;
+
+import br.ufscar.kdm_manager.core.readers.modelReader.factory.KDMModelReaderJavaFactory;
+import br.ufscar.kdm_manager.core.readers.relationshipReader.interfaces.KDMRelationshipGenericReader;
+
+public class KDMRelationshipReaderUsesTypeImpl implements KDMRelationshipGenericReader<UsesType> {
+
+	@Override
+	public List<UsesType> getAllRelationshipOf(Segment segmentToAvaliate) {
+		List<UsesType> usesTypeRecovered = new ArrayList<UsesType>();
+
+		Map<String, List<CodeModel>> models = KDMModelReaderJavaFactory.eINSTANCE.createKDMCodeModelReader().getAllFromSegment(segmentToAvaliate);
+
+		for (String nameCodeModel : models.keySet()) {
+
+			for (CodeModel codeModel : models.get(nameCodeModel)) {
+
+				usesTypeRecovered.addAll(this.getAllRelationshipOf(codeModel));
+			}
+		}
+
+		return usesTypeRecovered;
+	}
+
+	@Override
+	public List<UsesType> getAllRelationshipOf(CodeModel codeModelToAvaliate) {
+		List<UsesType> usesTypeRecovered = new ArrayList<UsesType>();
+		
+		for (AbstractCodeElement abstractCodeElement : codeModelToAvaliate.getCodeElement()) {
+			
+			if(abstractCodeElement instanceof InterfaceUnit){
+				usesTypeRecovered.addAll(this.getAllRelationshipOf((InterfaceUnit) abstractCodeElement));
+			}else if(abstractCodeElement instanceof EnumeratedType){
+				usesTypeRecovered.addAll(this.getAllRelationshipOf((EnumeratedType) abstractCodeElement));
+			}else if(abstractCodeElement instanceof ClassUnit){
+				usesTypeRecovered.addAll(this.getAllRelationshipOf((ClassUnit) abstractCodeElement));
+			}else if(abstractCodeElement instanceof Package){
+				usesTypeRecovered.addAll(this.getAllRelationshipOf((Package) abstractCodeElement));
+			}
+		}
+		
+		return usesTypeRecovered;
+	}
+	
+	@Override
+	public List<UsesType> getAllRelationshipOf(Package packageToAvaliate) {
+
+		List<UsesType> usesTypeRecovered = new ArrayList<UsesType>();
+		
+		for (AbstractCodeElement abstractCodeElement : packageToAvaliate.getCodeElement()) {
+			
+			if(abstractCodeElement instanceof ClassUnit){
+				usesTypeRecovered.addAll(this.getAllRelationshipOf((ClassUnit) abstractCodeElement));
+			}else if(abstractCodeElement instanceof InterfaceUnit){
+				usesTypeRecovered.addAll(this.getAllRelationshipOf((InterfaceUnit) abstractCodeElement));
+			}else if(abstractCodeElement instanceof EnumeratedType){
+				usesTypeRecovered.addAll(this.getAllRelationshipOf((EnumeratedType) abstractCodeElement));
+			}else if(abstractCodeElement instanceof Package){
+				usesTypeRecovered.addAll(this.getAllRelationshipOf((Package) abstractCodeElement));
+			}
+			
+		}
+		
+		return usesTypeRecovered;
+	}
+	
+	@Override
+	public List<UsesType> getAllRelationshipOf(ClassUnit classToAvaliate) {
+
+		List<UsesType> usesTypeRecovered = new ArrayList<UsesType>();
+		
+		for (CodeItem codeItem : classToAvaliate.getCodeElement()) {
+			
+			if(codeItem instanceof MethodUnit){
+				usesTypeRecovered.addAll(this.getAllRelationshipOf((MethodUnit) codeItem));
+			}else if(codeItem instanceof ClassUnit){
+				usesTypeRecovered.addAll(this.getAllRelationshipOf((ClassUnit) codeItem));
+			}else if(codeItem instanceof InterfaceUnit){
+				usesTypeRecovered.addAll(this.getAllRelationshipOf((InterfaceUnit) codeItem));
+			}else if(codeItem instanceof EnumeratedType){
+				usesTypeRecovered.addAll(this.getAllRelationshipOf((EnumeratedType) codeItem));
+			}
+			
+		}
+		
+		return usesTypeRecovered;
+	}
+	
+	@Override
+	public List<UsesType> getAllRelationshipOf(InterfaceUnit interfaceToAvaliate) {
+
+		List<UsesType> usesTypeRecovered = new ArrayList<UsesType>();
+		
+		for (CodeItem codeItem : interfaceToAvaliate.getCodeElement()) {
+			
+			if(codeItem instanceof MethodUnit){
+				usesTypeRecovered.addAll(this.getAllRelationshipOf((MethodUnit) codeItem));
+			}else if(codeItem instanceof ClassUnit){
+				usesTypeRecovered.addAll(this.getAllRelationshipOf((ClassUnit) codeItem));
+			}else if(codeItem instanceof InterfaceUnit){
+				usesTypeRecovered.addAll(this.getAllRelationshipOf((InterfaceUnit) codeItem));
+			}else if(codeItem instanceof EnumeratedType){
+				usesTypeRecovered.addAll(this.getAllRelationshipOf((EnumeratedType) codeItem));
+			}
+			
+		}
+		
+		return usesTypeRecovered;
+	}
+
+	@Override
+	public List<UsesType> getAllRelationshipOf(EnumeratedType enumeratedTypeToAvaliate) {
+		List<UsesType> usesTypeRecovered = new ArrayList<UsesType>();
+		
+		for (CodeItem codeItem : enumeratedTypeToAvaliate.getCodeElement()) {
+			
+			if(codeItem instanceof MethodUnit){
+				usesTypeRecovered.addAll(this.getAllRelationshipOf((MethodUnit) codeItem));
+			}else if(codeItem instanceof ClassUnit){
+				usesTypeRecovered.addAll(this.getAllRelationshipOf((ClassUnit) codeItem));
+			}else if(codeItem instanceof InterfaceUnit){
+				usesTypeRecovered.addAll(this.getAllRelationshipOf((InterfaceUnit) codeItem));
+			}else if(codeItem instanceof EnumeratedType){
+				usesTypeRecovered.addAll(this.getAllRelationshipOf((EnumeratedType) codeItem));
+			}
+			
+		}
+		
+		return usesTypeRecovered;
+	}
+
+	@Override
+	@Deprecated
+	public List<UsesType> getAllRelationshipOf(StorableUnit storableToAvaliate) {
+		return null;
+	}
+
+	@Override
+	public List<UsesType> getAllRelationshipOf(MethodUnit methodToAvaliate) {
+
+		List<UsesType> usesTypeRecovered = new ArrayList<UsesType>();
+		
+		for (AbstractCodeElement abstractCodeElement : methodToAvaliate.getCodeElement()) {
+			
+			if(abstractCodeElement instanceof BlockUnit){
+				usesTypeRecovered.addAll(this.getAllRelationshipOf((BlockUnit) abstractCodeElement));
+			}
+			
+		}
+		
+		return usesTypeRecovered;
+	}
+
+	@Override
+	@Deprecated
+	public List<UsesType> getAllRelationshipOf(Signature signatureToAvaliate) {
+		return null;
+	}
+
+	@Override
+	@Deprecated
+	public List<UsesType> getAllRelationshipOf(ParameterUnit parameterToAvaliate) {
+		return null;
+	}
+	
+	@Override
+	public List<UsesType> getAllRelationshipOf(BlockUnit blockToAvaliate) {
+		
+		List<UsesType> usesTypeRecovered = new ArrayList<UsesType>();
+
+		for (AbstractCodeElement abstractCodeElement : blockToAvaliate.getCodeElement()) {
+
+			if(abstractCodeElement instanceof ActionElement){
+
+				usesTypeRecovered.addAll(this.getAllRelationshipOf((ActionElement) abstractCodeElement));
+
+			}else if(abstractCodeElement instanceof BlockUnit){
+
+				usesTypeRecovered.addAll(this.getAllRelationshipOf((BlockUnit) abstractCodeElement));
+
+			}
+
+		}
+
+		return usesTypeRecovered;
+	}
+
+	@Override
+	public List<UsesType> getAllRelationshipOf(ActionElement actionElementToAvaliate) {
+		
+		List<UsesType> usesTypeRecovered = new ArrayList<UsesType>();
+
+		for (AbstractCodeRelationship abstractCodeRelationship : actionElementToAvaliate.getCodeRelation()) {
+			
+			if(abstractCodeRelationship instanceof UsesType){
+				usesTypeRecovered.add((UsesType) abstractCodeRelationship);
+			}
+			
+		}
+		
+		for (AbstractCodeElement abstractCodeElement : actionElementToAvaliate.getCodeElement()) {
+
+			if(abstractCodeElement instanceof ActionElement){
+
+				usesTypeRecovered.addAll(this.getAllRelationshipOf((ActionElement) abstractCodeElement));
+
+			}else if(abstractCodeElement instanceof BlockUnit){
+
+				usesTypeRecovered.addAll(this.getAllRelationshipOf((BlockUnit) abstractCodeElement));
+
+			}
+		}
+		
+		return usesTypeRecovered;
+	}
+
+}
