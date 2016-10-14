dc7f4231635310e2777335df1661719a59120d9c
André <a_souzalandy@yahoo.com.br>
2016-10-14 14:40:31 -0300

    Atividade 24 - Leitor de Classes do segment (raiz KDM)

---
+++ br.ufscar.kdm-manager.core/src/br/ufscar/kdm_manager/core/readers/codeReader/impl/readers/java/KDMClassReaderImpl.java
@@ -0,0 +1,278 @@
+package br.ufscar.kdm_manager.core.readers.codeReader.impl.readers.java;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.gmt.modisco.omg.kdm.action.ActionElement;
+import org.eclipse.gmt.modisco.omg.kdm.action.BlockUnit;
+import org.eclipse.gmt.modisco.omg.kdm.code.AbstractCodeElement;
+import org.eclipse.gmt.modisco.omg.kdm.code.ClassUnit;
+import org.eclipse.gmt.modisco.omg.kdm.code.CodeItem;
+import org.eclipse.gmt.modisco.omg.kdm.code.CodeModel;
+import org.eclipse.gmt.modisco.omg.kdm.code.EnumeratedType;
+import org.eclipse.gmt.modisco.omg.kdm.code.InterfaceUnit;
+import org.eclipse.gmt.modisco.omg.kdm.code.MethodUnit;
+import org.eclipse.gmt.modisco.omg.kdm.code.Package;
+import org.eclipse.gmt.modisco.omg.kdm.code.ParameterUnit;
+import org.eclipse.gmt.modisco.omg.kdm.code.Signature;
+import org.eclipse.gmt.modisco.omg.kdm.code.StorableUnit;
+import org.eclipse.gmt.modisco.omg.kdm.kdm.Segment;
+
+import br.ufscar.kdm_manager.core.readers.codeReader.interfaces.KDMCodeGenericReader;
+import br.ufscar.kdm_manager.core.readers.modelReader.factory.KDMModelReaderJavaFactory;
+
+public class KDMClassReaderImpl implements KDMCodeGenericReader<ClassUnit>{
+
+	private boolean hasNoFilter = false;
+	private boolean hasFilterName = false;
+
+	private String filterName = "";
+
+	public KDMClassReaderImpl() {
+		super();
+	}
+
+	public KDMClassReaderImpl(String elementName) {
+		this.hasNoFilter = false;
+		this.hasFilterName  = true;
+		this.filterName = elementName;
+	}
+	
+	private boolean validateFilter(ClassUnit elementToValidate) {
+		if(this.hasNoFilter){
+			return true;
+		}else if(this.hasFilterName){
+			if(elementToValidate.getName().equalsIgnoreCase(this.filterName)){
+				return true;
+			}
+		}
+		return false;
+	}	
+
+	@Override
+	public List<ClassUnit> getAllFrom(Segment segmentToSearch) {
+		
+		List<ClassUnit> classUnitRecovered = new ArrayList<ClassUnit>();
+
+		Map<String, List<CodeModel>> models = KDMModelReaderJavaFactory.eINSTANCE.createKDMCodeModelReader().getAllFromSegment(segmentToSearch);
+
+		for (String nameCodeModel : models.keySet()) {
+
+			for (CodeModel codeModel : models.get(nameCodeModel)) {
+
+				classUnitRecovered.addAll(this.getAllClassesFrom(codeModel));
+			}
+		}
+		
+		return classUnitRecovered;
+	}
+
+	@Override
+	public List<ClassUnit> getAllFrom(CodeModel codeModelToSearch) {
+		return this.getAllClassesFrom(codeModelToSearch);
+	}
+
+	@Override
+	public List<ClassUnit> getAllFrom(Package packageToSearch) {
+		return this.getAllClassesFrom(packageToSearch);
+	}
+
+	@Override
+	public List<ClassUnit> getAllFrom(ClassUnit classUnitToSearch) {
+		return this.getAllClassesFrom(classUnitToSearch);
+	}
+
+	@Override
+	public List<ClassUnit> getAllFrom(InterfaceUnit interfaceUnitToSearch) {
+		return this.getAllClassesFrom(interfaceUnitToSearch);
+	}
+
+	@Override
+	public List<ClassUnit> getAllFrom(EnumeratedType enumeratedTypeToSearch) {
+		return this.getAllClassesFrom(enumeratedTypeToSearch);
+	}
+
+	@Override
+	@Deprecated
+	public List<ClassUnit> getAllFrom(MethodUnit methodUnitToSearch) {
+		return null;
+	}
+
+	@Override
+	@Deprecated
+	public List<ClassUnit> getAllFrom(StorableUnit storableUnitToSearch) {
+		return null;
+	}
+
+	@Override
+	@Deprecated
+	public List<ClassUnit> getAllFrom(BlockUnit blockUnitToSearch) {
+		return null;
+	}
+
+	@Override
+	@Deprecated
+	public List<ClassUnit> getAllFrom(Signature signatureUnitToSearch) {
+		return null;
+	}
+
+	@Override
+	@Deprecated
+	public List<ClassUnit> getAllFrom(ActionElement actionElementToSearch) {
+		return null;
+	}
+
+	@Override
+	@Deprecated
+	public List<ClassUnit> getAllFrom(ParameterUnit parameterUnitUnitToSearch) {
+		return null;
+	}
+	
+	
+	private List<ClassUnit> getAllClassesFrom(CodeModel codeModelToSearch) {
+		List<ClassUnit> allClassUnits = new ArrayList<ClassUnit>();
+
+		for (AbstractCodeElement abstractCodeElement : codeModelToSearch.getCodeElement()) {
+			
+			if(abstractCodeElement instanceof ClassUnit){
+				
+				if (validateFilter((ClassUnit) abstractCodeElement)) {
+					allClassUnits.add((ClassUnit) abstractCodeElement);
+				}
+				allClassUnits.addAll(this.getAllClassesFrom((ClassUnit) abstractCodeElement));
+				
+			}else if(abstractCodeElement instanceof InterfaceUnit){
+				
+				allClassUnits.addAll(this.getAllClassesFrom((InterfaceUnit) abstractCodeElement));
+				
+			}else if(abstractCodeElement instanceof EnumeratedType){
+				
+				allClassUnits.addAll(this.getAllClassesFrom((EnumeratedType) abstractCodeElement));
+				
+			}else if(abstractCodeElement instanceof Package){
+				
+				allClassUnits.addAll(this.getAllClassesFrom((Package) abstractCodeElement));
+				
+			}
+		
+		
+		}
+		
+		return allClassUnits;
+	}
+
+	private List<ClassUnit> getAllClassesFrom(ClassUnit classUnitToSearch) {
+		List<ClassUnit> allClassUnits = new ArrayList<ClassUnit>();
+
+		for (CodeItem codeItem : classUnitToSearch.getCodeElement()) {
+			
+			if(codeItem instanceof ClassUnit){
+				
+				if (validateFilter((ClassUnit) codeItem)) {
+					allClassUnits.add((ClassUnit) codeItem);
+				}
+				allClassUnits.addAll(this.getAllClassesFrom((ClassUnit) codeItem));
+				
+			}else if(codeItem instanceof InterfaceUnit){
+				
+				allClassUnits.addAll(this.getAllClassesFrom((InterfaceUnit) codeItem));
+			
+			}else if (codeItem instanceof EnumeratedType) {
+				
+				allClassUnits.addAll(this.getAllClassesFrom((EnumeratedType) codeItem));
+				
+			}
+		
+		}
+		
+		return allClassUnits;
+	}
+
+
+	private List<ClassUnit> getAllClassesFrom(InterfaceUnit interfaceUnitToSearch) {
+		List<ClassUnit> allClassUnits = new ArrayList<ClassUnit>();
+
+		for (CodeItem codeItem : interfaceUnitToSearch.getCodeElement()) {
+			
+			if(codeItem instanceof ClassUnit){
+				
+				if (validateFilter((ClassUnit) codeItem)) {
+					allClassUnits.add((ClassUnit) codeItem);
+				}
+				allClassUnits.addAll(this.getAllClassesFrom((ClassUnit) codeItem));
+				
+			}else if(codeItem instanceof InterfaceUnit){
+				
+				allClassUnits.addAll(this.getAllClassesFrom((InterfaceUnit) codeItem));
+			
+			}else if (codeItem instanceof EnumeratedType) {
+				
+				allClassUnits.addAll(this.getAllClassesFrom((EnumeratedType) codeItem));
+				
+			}
+			
+		}
+		
+		return allClassUnits;
+	}
+	
+	private List<ClassUnit> getAllClassesFrom(EnumeratedType enumeratedTypeToSearch) {
+		List<ClassUnit> allClassUnits = new ArrayList<ClassUnit>();
+
+		for (CodeItem codeItem : enumeratedTypeToSearch.getCodeElement()) {
+			
+			if(codeItem instanceof ClassUnit){
+				
+				if (validateFilter((ClassUnit) codeItem)) {
+					allClassUnits.add((ClassUnit) codeItem);
+				}
+				allClassUnits.addAll(this.getAllClassesFrom((ClassUnit) codeItem));
+				
+			}else if(codeItem instanceof InterfaceUnit){
+				
+				allClassUnits.addAll(this.getAllClassesFrom((InterfaceUnit) codeItem));
+			
+			}else if (codeItem instanceof EnumeratedType) {
+				
+				allClassUnits.addAll(this.getAllClassesFrom((EnumeratedType) codeItem));
+				
+			}
+		
+		}
+		
+		return allClassUnits;
+	}
+	
+	private List<ClassUnit> getAllClassesFrom(Package packageToSearch) {
+		List<ClassUnit> allClassUnits = new ArrayList<ClassUnit>();
+
+		for (AbstractCodeElement abstractCodeElement : packageToSearch.getCodeElement()) {
+			
+			if(abstractCodeElement instanceof ClassUnit){
+				
+				if (validateFilter((ClassUnit) abstractCodeElement)) {
+					allClassUnits.add((ClassUnit) abstractCodeElement);
+				}
+				allClassUnits.addAll(this.getAllClassesFrom((ClassUnit) abstractCodeElement));
+				
+			}else if(abstractCodeElement instanceof InterfaceUnit){
+				
+				allClassUnits.addAll(this.getAllClassesFrom((InterfaceUnit) abstractCodeElement));
+			
+			}else if (abstractCodeElement instanceof EnumeratedType) {
+				
+				allClassUnits.addAll(this.getAllClassesFrom((EnumeratedType) abstractCodeElement));
+				
+			}else if (abstractCodeElement instanceof Package) {
+				
+				allClassUnits.addAll(this.getAllClassesFrom((Package) abstractCodeElement));
+
+			}
+		
+		}
+		
+		return allClassUnits;
+	}
+
+}
