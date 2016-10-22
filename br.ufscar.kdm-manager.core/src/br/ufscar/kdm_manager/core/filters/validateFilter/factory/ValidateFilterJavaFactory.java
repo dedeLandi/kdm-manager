1251a08a8ceb22299b2fc7d7a79a87a2bf00966c
André <a_souzalandy@yahoo.com.br>
2016-10-22 17:13:22 -0200

    Atividade 90 - Estrutura Basica do filter de validação
    acrescimo dos novos filtros

--- br.ufscar.kdm-manager.core/src/br/ufscar/kdm_manager/core/filters/validateFilter/factory/ValidateFilterJavaFactory.java
+++ br.ufscar.kdm-manager.core/src/br/ufscar/kdm_manager/core/filters/validateFilter/factory/ValidateFilterJavaFactory.java
@@ -20,6 +20,8 @@
   *******************************************************************************/
 package br.ufscar.kdm_manager.core.filters.validateFilter.factory;
 
+import org.eclipse.gmt.modisco.omg.kdm.core.KDMEntity;
+
 import br.ufscar.kdm_manager.core.filters.validateFilter.interfaces.ValidateFilter;
 
 public interface ValidateFilterJavaFactory {
@@ -29,5 +31,11 @@ public interface ValidateFilterJavaFactory {
 	ValidateFilter<?, String> createValidateFilterNameOfKDMEntity(String nameToSearch);
 	
 	ValidateFilter<?, String> createValidateFilterNameOfKDMFramework(String nameToSearch);
+	
+	ValidateFilter<?, KDMEntity> createValidateFilterAggregatedRelationshipToOfKDMEntity(KDMEntity elementToAvaliate);
+	
+	ValidateFilter<?, KDMEntity> createValidateFilterAggregatedRelationshipFromOfKDMEntity(KDMEntity elementToAvaliate);
+	
+	ValidateFilter<?, KDMEntity[]> createValidateFilterAggregatedRelationshipToFromOfKDMEntity(KDMEntity[] elementToAvaliate);
 	
 }
