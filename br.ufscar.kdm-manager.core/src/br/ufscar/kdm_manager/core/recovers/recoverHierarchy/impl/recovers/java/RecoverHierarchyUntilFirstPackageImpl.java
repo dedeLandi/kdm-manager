0729c3231d5892c8f48a41676f4c90d24cdd5c94
André <a_souzalandy@yahoo.com.br>
2016-11-16 13:32:14 -0200

    Atividade 71 - Recover de Hierarquia do tipo Até o primeiro Pacote
    Acrescentar leitura para try Catch

--- br.ufscar.kdm-manager.core/src/br/ufscar/kdm_manager/core/recovers/recoverHierarchy/impl/recovers/java/RecoverHierarchyUntilFirstPackageImpl.java
+++ br.ufscar.kdm-manager.core/src/br/ufscar/kdm_manager/core/recovers/recoverHierarchy/impl/recovers/java/RecoverHierarchyUntilFirstPackageImpl.java
@@ -22,6 +22,8 @@ package br.ufscar.kdm_manager.core.recovers.recoverHierarchy.impl.recovers.java;
 
 import org.eclipse.gmt.modisco.omg.kdm.action.ActionElement;
 import org.eclipse.gmt.modisco.omg.kdm.action.BlockUnit;
+import org.eclipse.gmt.modisco.omg.kdm.action.CatchUnit;
+import org.eclipse.gmt.modisco.omg.kdm.action.TryUnit;
 import org.eclipse.gmt.modisco.omg.kdm.code.ClassUnit;
 import org.eclipse.gmt.modisco.omg.kdm.code.EnumeratedType;
 import org.eclipse.gmt.modisco.omg.kdm.code.InterfaceUnit;
@@ -71,6 +73,12 @@ public class RecoverHierarchyUntilFirstPackageImpl implements RecoverGenericHier
 
 		if(blockToAvaliate.eContainer() instanceof MethodUnit){
 			completePath = completePath.concat(this.getHierarchyOf((MethodUnit) blockToAvaliate.eContainer()));
+		}else if(blockToAvaliate.eContainer() instanceof TryUnit){
+			completePath = completePath.concat(this.getHierarchyOf((TryUnit) blockToAvaliate.eContainer()));
+		}else if(blockToAvaliate.eContainer() instanceof CatchUnit){
+			completePath = completePath.concat(this.getHierarchyOf((CatchUnit) blockToAvaliate.eContainer()));
+		}else if(blockToAvaliate.eContainer() instanceof BlockUnit){
+			completePath = completePath.concat(this.getHierarchyOf((BlockUnit) blockToAvaliate.eContainer()));
 		}else if(blockToAvaliate.eContainer() instanceof ActionElement){
 			completePath = completePath.concat(this.getHierarchyOf((ActionElement) blockToAvaliate.eContainer()));
 		}
@@ -87,6 +95,10 @@ public class RecoverHierarchyUntilFirstPackageImpl implements RecoverGenericHier
 
 		if(actionElementToAvaliate.eContainer() instanceof BlockUnit){
 			completePath = completePath.concat(this.getHierarchyOf((BlockUnit) actionElementToAvaliate.eContainer()));
+		}else if(actionElementToAvaliate.eContainer() instanceof TryUnit){
+			completePath = completePath.concat(this.getHierarchyOf((TryUnit) actionElementToAvaliate.eContainer()));
+		}else if(actionElementToAvaliate.eContainer() instanceof CatchUnit){
+			completePath = completePath.concat(this.getHierarchyOf((CatchUnit) actionElementToAvaliate.eContainer()));
 		}else if(actionElementToAvaliate.eContainer() instanceof ActionElement){
 			completePath = completePath.concat(this.getHierarchyOf((ActionElement) actionElementToAvaliate.eContainer()));
 		}
@@ -141,6 +153,10 @@ public class RecoverHierarchyUntilFirstPackageImpl implements RecoverGenericHier
 			completePath = completePath.concat(this.getHierarchyOf((EnumeratedType) storableToAvaliate.eContainer()));
 		}else if(storableToAvaliate.eContainer() instanceof BlockUnit){
 			completePath = completePath.concat(this.getHierarchyOf((BlockUnit) storableToAvaliate.eContainer()));
+		}else if(storableToAvaliate.eContainer() instanceof TryUnit){
+			completePath = completePath.concat(this.getHierarchyOf((TryUnit) storableToAvaliate.eContainer()));
+		}else if(storableToAvaliate.eContainer() instanceof CatchUnit){
+			completePath = completePath.concat(this.getHierarchyOf((CatchUnit) storableToAvaliate.eContainer()));
 		}else if(storableToAvaliate.eContainer() instanceof ActionElement){
 			completePath = completePath.concat(this.getHierarchyOf((ActionElement) storableToAvaliate.eContainer()));
 		}
