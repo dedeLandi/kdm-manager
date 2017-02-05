59e40ef0b03212f1155b1689b5accc692a8bc76c
André <a_souzalandy@yahoo.com.br>
2017-02-05 16:36:24 -0200

    Atividade 123 - Implementação do Factory da Text metric Engine

---
+++ br.ufscar.kdm-manager.core/src/br/ufscar/kdm_manager/core/executionEngines/metricsEngine/textMetricEngine/impl/factory/TextMetricsEngineFactoryImpl.java
@@ -0,0 +1,61 @@
+/********************************************************************************
+ *																				*
+ * Copyright (c) 2016, André de Souza Landi. All rights reserved.				*
+ *																				*
+ * This file is part of KDM-MANAGER software.									*
+ *																				*
+ * KDM-MANAGER is free software: you can redistribute it and/or modify			*
+ * it under the terms of the GNU General Public License as published by			*
+ * the Free Software Foundation, either version 3 of the License, or			*
+ * (at your option) any later version.											*
+ *																				*
+ * KDM-MANAGER is distributed in the hope that it will be useful,				*
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of				*
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the				*
+ * GNU General Public License for more details.									*
+ *																				*
+ * You should have received a copy of the GNU General Public License			*
+ * along with KDM-MANAGER.  If not, see <http://www.gnu.org/licenses/>.			*
+ *																				*
+  *******************************************************************************/
+package br.ufscar.kdm_manager.core.executionEngines.metricsEngine.textMetricEngine.impl.factory;
+
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.impl.EFactoryImpl;
+import org.eclipse.emf.ecore.plugin.EcorePlugin;
+
+import br.ufscar.kdm_manager.core.executionEngines.metricsEngine.textMetricEngine.factory.TextMetricsEngineFactory;
+import br.ufscar.kdm_manager.core.executionEngines.metricsEngine.textMetricEngine.impl.textMetricEngine.TextMetricLCSbyThibaultDebatty;
+import br.ufscar.kdm_manager.core.executionEngines.metricsEngine.textMetricEngine.interfaces.TextMestricEngine;
+import info.debatty.java.stringsimilarity.MetricLCS;
+
+/**
+ * @author Landi
+ *
+ */
+public class TextMetricsEngineFactoryImpl extends EFactoryImpl implements TextMetricsEngineFactory{
+
+	public static TextMetricsEngineFactory init() {
+		try {
+			TextMetricsEngineFactory theTextMetricsEngineFactory = (TextMetricsEngineFactory)EPackage.Registry.INSTANCE.getEFactory("br.ufscar.kdm_manager.core.executionEngines.metricsEngine.textMetricEngine.factory.TextMetricsEngineFactory"); //$NON-NLS-1$ 
+			if (theTextMetricsEngineFactory != null) {
+				return theTextMetricsEngineFactory;
+			}
+		}
+		catch (Exception exception) {
+			EcorePlugin.INSTANCE.log(exception);
+		}
+		return new TextMetricsEngineFactoryImpl();
+	}
+
+	/* (non-Javadoc)
+	 * @see br.ufscar.kdm_manager.core.executionEngines.metricsEngine.textMetricEngine.factory.TextMetricsEngineFactory#createTextMetricLongestCommonSubsequence()
+	 */
+	@Override
+	public TextMestricEngine<MetricLCS, Double> createTextMetricLongestCommonSubsequence() {
+		TextMetricLCSbyThibaultDebatty metric = new TextMetricLCSbyThibaultDebatty();
+		return metric;
+	}
+
+	
+}
